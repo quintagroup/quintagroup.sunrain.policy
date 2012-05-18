30c3f75d407ceb6c34e861e47b74e3d610fab681
naivok <naivok@quintagroup.com>
2012-05-18 07:40:38 +0000

    changed txt files

--- setup.py
+++ setup.py
@@ -6,18 +6,21 @@ version = '1.0'
 setup(name='quintagroup.sunrain.policy',
       version=version,
       description="",
-      long_description=open(os.path.join("quintagroup", "sunrain", "policy", "README.txt")).read() + "\n" +
+      long_description=open(os.path.join("quintagroup", "sunrain", "policy", "README.txt")).read() + "\n\n" +
+                       open(os.path.join("docs", "INSTALL.txt")).read() + "\n\n"+
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
+        'Framework :: Plone',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
         ],
-      keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
+      keywords='web zope plone theme diazo quintagroup',
+      author='Quintagroup',
+      author_email='skins@quintagroup.com',
+      url='http://skins.quintagroup.com/sunrain',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['quintagroup', 'quintagroup.sunrain'],
